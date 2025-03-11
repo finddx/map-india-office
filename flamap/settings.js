1987c1ddb4f32df9a6cde94c3ae84d504cf7338c
Ben Ubah <ben.ubah@Finddx.org>
2025-03-11 10:51:22 +0100

    use flamap instead

---
+++ flamap/settings.js
@@ -0,0 +1,683 @@
+var map_cfg = {
+ "mapWidth": "100%",
+ "mapHeight": 500,
+ "shadowWidth": 2,
+ "shadowOpacity": 0.2,
+ "shadowColor": "black",
+ "shadowX": 0,
+ "shadowY": 0,
+ "iPhoneLink": true,
+ "isNewWindow": false,
+ "zoomEnable": false,
+ "zoomEnableControls": false,
+ "zoomIgnoreMouseScroll": false,
+ "zoomMax": 8,
+ "zoomStep": 0.8,
+ "borderColor": "#ffffff",
+ "borderColorOver": "#ffffff",
+ "borderOpacity": 0.5,
+ "nameColor": "#ffffff",
+ "nameColorOver": "#ffffff",
+ "nameFontSize": "10px",
+ "nameFontWeight": "bold",
+ "nameStroke": true,
+ "nameStrokeColor": "#000000",
+ "nameStrokeColorOver": "#000000",
+ "nameStrokeWidth": 1.5,
+ "nameStrokeOpacity": 0.5,
+ "overDelay": 300,
+ "nameAutoSize": false,
+ "tooltipOnHighlightIn": false,
+ "freezeTooltipOnClick": false,
+ "mapId": "7f5w8",
+ "map_data": {
+  "st1": {
+   "name": "Andaman and Nicobar Islands",
+   "shortname": "AN",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st2": {
+   "name": "Andhra Pradesh",
+   "shortname": "AP",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st3": {
+   "name": "Arunachal Pradesh",
+   "shortname": "AR",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st4": {
+   "name": "Assam",
+   "shortname": "AS",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st5": {
+   "name": "Bihar",
+   "shortname": "BR",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st6": {
+   "name": "Chandigarh",
+   "shortname": "CH",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st7": {
+   "name": "Chhattisgarh",
+   "shortname": "CT",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st8": {
+   "name": "Dadra and Nagar Haveli and Daman and Diu",
+   "shortname": "DD",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st9": {
+   "name": "National Capital Territory of Delhi",
+   "shortname": "DL",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st10": {
+   "name": "Goa",
+   "shortname": "GA",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st11": {
+   "name": "Gujarat",
+   "shortname": "GJ",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st12": {
+   "name": "Himachal Pradesh",
+   "shortname": "HP",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st13": {
+   "name": "Haryana",
+   "shortname": "HR",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st14": {
+   "name": "Jharkhand",
+   "shortname": "JH",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st15": {
+   "name": "Jammu and Kashmir",
+   "shortname": "JK",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st16": {
+   "name": "Karnataka",
+   "shortname": "KA",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st17": {
+   "name": "Kerala",
+   "shortname": "KL",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st18": {
+   "name": "Ladakh",
+   "shortname": "LA",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st19": {
+   "name": "Lakshadweep",
+   "shortname": "LD",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st20": {
+   "name": "Maharashtra",
+   "shortname": "MH",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st21": {
+   "name": "Meghalaya",
+   "shortname": "ML",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st22": {
+   "name": "Manipur",
+   "shortname": "MN",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st23": {
+   "name": "Madhya Pradesh",
+   "shortname": "MP",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st24": {
+   "name": "Mizoram",
+   "shortname": "MZ",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st25": {
+   "name": "Nagaland",
+   "shortname": "NL",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st26": {
+   "name": "Orissa",
+   "shortname": "OR",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st27": {
+   "name": "Punjab",
+   "shortname": "PB",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st28": {
+   "name": "Puducherry",
+   "shortname": "PY",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st29": {
+   "name": "Rajasthan",
+   "shortname": "RJ",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st30": {
+   "name": "Sikkim",
+   "shortname": "SK",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st31": {
+   "name": "Telangana",
+   "shortname": "TG",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st32": {
+   "name": "Tamil Nadu",
+   "shortname": "TN",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st33": {
+   "name": "Tripura",
+   "shortname": "TR",
+   "comment": "",
+   "link": "",
+   "color": "#81969f",
+   "colorOver": "#81969f"
+  },
+  "st34": {
+   "name": "Uttar Pradesh",
+   "shortname": "UP",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st35": {
+   "name": "Uttarakhand",
+   "shortname": "UT",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  },
+  "st36": {
+   "name": "West Bengal",
+   "shortname": "WB",
+   "comment": "",
+   "link": "",
+   "color": "#489fa9",
+   "colorOver": "#81969f"
+  }
+ },
+ "points": {
+  "p0": {
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "Tuberculosis",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p1": {
+   "x": 167.785,
+   "y": 223.072,
+   "color": "#e52a2a",
+   "colorOver": "#E06666",
+   "name": "AMR",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p2": {
+   "x": 302.143,
+   "y": 225,
+   "color": "#e52a2a",
+   "colorOver": "#E06666",
+   "name": "AMR",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p3": {
+   "x": 213.429,
+   "y": 255.214,
+   "color": "#e52a2a",
+   "colorOver": "#E06666",
+   "name": "AMR",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p4": {
+   "x": 199.929,
+   "y": 87.429,
+   "color": "#e52a2a",
+   "colorOver": "#E06666",
+   "name": "AMR",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p5": {
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "TB",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p6": {
+   "x": 124.714,
+   "y": 106.714,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p7": {
+   "x": 174.857,
+   "y": 120.857,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p8": {
+   "x": 136.285,
+   "y": 133.072,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p9": {
+   "x": 58.5,
+   "y": 126,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p10": {
+   "x": 203.143,
+   "y": 172.286,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p11": {
+   "x": 176.786,
+   "y": 306,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p12": {
+   "x": 103.5,
+   "y": 169.072,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p13": {
+   "x": 71.357,
+   "y": 230.143,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p14": {
+   "x": 115.714,
+   "y": 360.643,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p15": {
+   "x": 127.285,
+   "y": 282.214,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p16": {
+   "x": 169.714,
+   "y": 354.214,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p17": {
+   "x": 160.071,
+   "y": 418.499,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p18": {
+   "x": 279,
+   "y": 188.357,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p19": {
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "",
+   "shortname": "TB",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p20": {
+   "x": 257.786,
+   "y": 266.143,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p22": {
+   "x": 369.642,
+   "y": 171,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p23": {
+   "x": 269.357,
+   "y": 187.715,
+   "color": "#2ae57b",
+   "colorOver": "#2ae57b",
+   "name": "HSS",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p24": {
+   "x": 143.357,
+   "y": 133.071,
+   "color": "#e5c52a",
+   "colorOver": "#e5c529",
+   "name": "Women's Health",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p25": {
+   "x": 124.071,
+   "y": 360,
+   "color": "#e5c52a",
+   "colorOver": "#e5c529",
+   "name": "Women's Health",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p26": {
+   "x": 339.429,
+   "y": 204.428,
+   "color": "#872ae5",
+   "colorOver": "#882ae4",
+   "name": "Hep-C",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p27": {
+   "x": 133.072,
+   "y": 106.071,
+   "color": "#872ae5",
+   "colorOver": "#882ae4",
+   "name": "Hep-C",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p28": {
+   "x": 150.429,
+   "y": 94.5,
+   "color": "#2a30e5",
+   "colorOver": "#2a30e5",
+   "name": "TB",
+   "shortname": "",
+   "pointType": "marker",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  }
+ }
+};
